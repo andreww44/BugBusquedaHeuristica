86c898ca48ae88bbce22f941d1a65f17fe4fc9bd
andreww44 <monsalve.andres.salas@gmail.com>
2024-09-01 22:49:43 -0400

    01/09/2024
    Imprimir por pantalla y cambiar a numeros binarios

--- BugPrograma/mainbug.cpp
+++ BugPrograma/mainbug.cpp
@@ -1,94 +1,141 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <bitset>
 
 //Coordenadas para cada punto del tablero
-int _cq[19] = {0,1,2,-1,0,1,2,-2,-1,0,1,2,-2,-1,0,1,-2,-1,0};
-int _cr[19] = {-2,-2,-2,-1,-1,-1,-1,0,0,0,0,0,1,1,1,1,2,2,2}; 
-int _cs[19] = {2,1,0,2,1,0,-1,2,1,0,-1,-2,1,0,-1,-2,0,-1,-2}; 
+unsigned int w_table = 0x0000000;
+unsigned int b_table = 0x0000000;
 
-// Define el tamaño del tablero
-//Formula para estimar el tamaño del tablero. 3n(n−1)+1
-//const int WIDTH = 3; // Ancho del tablero
-
-//Estructura que guarda las coordenas axiales de cada punto
-struct Hex {
-    int q; // Coordenada xial Q
-    int r; // Coordenada Axial R
-    int s; // Coordenada Axial S
-    bool occupied; // Estado del hexágono (por ejemplo, si está ocupado o no)
-};
-
-
-//En proceso función para llenar con las coordenadas de cada pieza correspondiente.
-void fillHexBoard(std::vector<Hex>& board) {
-    
-    for (int index = 0; index < board.size(); index++) {
-        board[index].q = _cq[index];
-        board[index].r = _cr[index];
-        board[index].s = _cs[index];   
-    }
-}
-
-void printTableBug(int w){
-    
-    //char none = ;
-    int it = 2*w -1;
-    for (int i = 0; i < it; i++)
-    {
-        for(int j = 0; j < it; j++)
-        {
-            if(i == 0 || i == it-1){
-                if(j== 0 || j == 1){
+//Funcion que imprime ambos tableros
+void printBoard(){
+    int count = 1;
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            if(i == 0){
+                if(j == 0 || j == 1){
                     std::cout << " ";
+                }
+                else if(w_table& (1 << j+14))
+                {
+                    std::cout << "W ";
+                }
+                else if(b_table&(1<<j+14)){
+                    std::cout << "B ";
                 }
                 else{
                     std::cout << "* ";
                 }
             }
-            if(i == 1 || i == it-2){
-                if(j== 0){
+            if(i== 1){
+                if(j == 0){
                     std::cout << " ";
+                }
+                else if(w_table& (1 << j+11))
+                {
+                    std::cout << "W ";
+                }
+                else if(b_table&(1<<j+11)){
+                    std::cout << "B ";
                 }
                 else{
                     std::cout << "* ";
                 }
             }
             if(i == 2){
-                std::cout << "* ";
+                if(w_table& (1 << j+7)){
+                    std::cout << "W ";
+                }
+                else if(b_table& (1 << j+7))
+                {
+                    std::cout << "B ";
+                }
+                else{
+                    std::cout << "* ";
+                }
             }
-            
+            if(i == 3){
+                if(j == 0){
+                    std::cout << " ";
+                }
+                else if(w_table& (1 << j+2))
+                {
+                    std::cout << "W ";
+                }
+                else if(b_table& (1 << j+2)){
+                    std::cout << "B ";
+                }
+                else{
+                    std::cout << "* ";
+                }
+            }
+            if(i == 4){
+                if(j == 0 || j == 1){
+                    std::cout << " ";
+                }
+                else if(w_table& (1 << j-2))
+                {
+                    std::cout << "W ";
+                }
+                else if(b_table& (1 << j-2))
+                {
+                    std::cout << "B ";
+                }
+                else{
+                    std::cout << "* ";
+                }
+            }
         }
         std::cout << "\n";
     }
-    
-    //std::cout << it << "\n";
-
 }
 
-int main() {
-    int width = 3;
+int main()
+{
+    unsigned int hexValue = 0x02;
+    //Valor Maximo para el enmascaramiento
+    unsigned int hexMax = 0x0007FFFF;
+    //Tablero de las blancas
+    unsigned int hexWhite = 0x0;
+    //Tablero de las negras
+    unsigned int hexBlack = 0x0;
+    /*
+    std::cout << "Valor Hexadecimal = " << std::hex << table << std::endl;
+    std::bitset<32> bits(table);
+    std::cout << "Valor en bits = " << bits << std::endl;
+    //Tablero 
+    std::cout << "Valor Hexadecimal Maximo = " << std::hex <<hexMax << std::endl;
+    std::bitset<32> blackbits(hexMax);
+    std::cout << "Valor en Bits Maximo = " << blackbits << std::endl;
+*/
+    //Ciclo de juego
     
-    std::cout<<"Tamaño del tablero de 3x3x3x3x3 - Bug\n";
-    
-    int size = 3 * width * (width - 1)+1;
+    int a = -1;
 
-    std::vector<Hex> board(size);
+    unsigned int up = 0x0;
+    while (a != 0)
+    {
+        std::cout << "Bienvenido a Bug" << std::endl;
+        std::cout << "Ingrese en que posicion del 1 al 19 quiere ingresar a la ficha" << std::endl;
+        std::cout << "Ingrese 0 para salir" << std::endl;
+        std::cin >> a;
 
-    std::cout << board.size() << + "\n";
-    // Inicializa el tablero
+        unsigned int newPiece = 0x0000001;
 
-    fillHexBoard(board);
+        newPiece = 1 << a-1;
 
-    // Imprimir las coordenadas de las piezas
-    for (int i = 0; i < size; i++) {
-        std::cout << "Pieza " << i << ": (" 
-                  << board[i].q << ", " 
-                  << board[i].r << ", " 
-                  << board[i].s << ")\n";
+        std::bitset<32> bits(newPiece);
+        std::cout << "Valor en bits = " << bits << std::endl;
+        
+        if(a!=0){
+            up = newPiece;
+            w_table = w_table | up;
+            printBoard();
+        }
     }
-
-    printTableBug(width);
-
+    
+    w_table = up;
+    printBoard();
+    
     return 0;
 }
